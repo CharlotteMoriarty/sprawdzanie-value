3687e15f7b839de05297e38de858690d964259fd
CharlotteMoriarty <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-11-01 08:42:45 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+a=0;
+b=10;
+var value=((a * a) - (2 * a * b) + (b * b));
+//tak dla pewności
+//var value=(a-b); 
+console.log(value);
+if  ( value > 0) {
+  console.log('wynik dodatni');
+}
+  else {
+    console.log('wynik ujemny');
+  }
+
