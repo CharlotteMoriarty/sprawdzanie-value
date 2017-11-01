cae0f836e1e6ea221b7d3966f30144710604444f
CharlotteMoriarty <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-11-01 16:47:55 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 
-var a=1;
-var b=10;
+var a=2;
+var b=2;
 var value=((a * a) - (2 * a * b) + (b * b));
 //tak dla pewności
 //var value=(a-b); 
 console.log(value);
-if  ( value >= 0) {
+if  ( value > 0) {
   console.log('wynik dodatni');
 }
-  else {
-    console.log('wynik ujemny');
-  }
+//dla wyników ujemnych lub równych zero
+else if (value==0){
+  console.log('masz wynik zero');
+}
+  else  {
+    console.log('wynik ujemny'); 
+  } 
